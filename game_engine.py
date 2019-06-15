19d2c76546b472127717841e651a3c95f31e787d
Joseph Ball <joseph.ball@packetsolutions.io>
2019-06-15 13:01:37 +0100

    Add constructor support for tuple and list
    Change the default constructor to check the type of the board. It now
    supports passing a list or tuple instead of a filename.

--- game_engine.py
+++ game_engine.py
@@ -4,12 +4,20 @@
 
 class Sudoku():
     """Class implementation of a Sudoku game board"""
-    def __init__(self, board_file='board.txt'):
-        file_handler = open(board_file, 'r')
-        self.board_init = tuple(map(
-            int, file_handler.read().replace(',', '').split()))
-        self.board_current = list(self.board_init)
-        file_handler.close()
+    def __init__(self, board=None):
+        if isinstance(board, str):
+            file_handler = open(board, 'r')
+            self.board_init = tuple(map(
+                int, file_handler.read().replace(',', '').split()))
+            file_handler.close()
+        if isinstance(board, tuple):
+            self.board_init = board
+        if isinstance(board, list):
+            self.board_init = tuple(board)
+        if len(self.board_init) == 81:
+            self.board_current = list(self.board_init)
+        else:
+            raise IndexError('Board must have exactly 81 squares')
 
     def __str__(self, board='board_current'):
         """Print the current board"""
